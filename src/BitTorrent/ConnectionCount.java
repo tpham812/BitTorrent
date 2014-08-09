85be0d78287f8ae8300c72713b9c221026fd7174
jklimmm <lim.jewel@gmail.com>
2014-08-09 14:40:32 -0400

    changes to upload and connectionCount

--- src/BitTorrent/ConnectionCount.java
+++ src/BitTorrent/ConnectionCount.java
@@ -2,6 +2,9 @@ package BitTorrent;
 
 public class ConnectionCount {
 
+	public static int uploadConnections;
+	public static int downloadConnections;
+
 	//data structure that is counting uploads
 	//data structure that is counting download
 	//keeps list of all peers connected
--- src/BitTorrent/Upload.java
+++ src/BitTorrent/Upload.java
@@ -102,29 +102,39 @@ public class Upload implements Runnable{
 
 
 
-	//if user has bitfield or have messages and is interested, unchoke.
-	//This does not account for the optimistically unchoked. Implement 30sec thread!
 	public boolean unchoke() throws IOException{
 
 		//Keep track of uploaded peers, call ConnectionCount
 		//If Upload is already 6, then we cannot unchoke anyone
-		//UNLESS 30 seconds has already passed. 
-		
-		
+		//If less than 3 download peers and peer is interested, let connect
+		//If 30 second timer wakes, choke worst peer and unchoke randomly. 
+
+		//read message from peer to see if they are interested.
 		int msgIDfrPeer = Peer.readMessage();
 		if (msgIDfrPeer == Message.MSG_INTERESTED){
-			System.out.println("Peer is interested.");	
-			int msgIDfrPeer2 = Peer.readMessage();  			
-			//byte[] bitFieldOrHaveMsg = new byte[in.available()];
-			//in.readFully(bitFieldOrHaveMsg); /**get rid of bit field*/  			
-			if(msgIDfrPeer2 == Message.MSG_HAVE || msgIDfrPeer2 == Message.MSG_BITFIELD){
-				System.out.println("Peer has something to share! Do not choke.");
-				return true;	
+			System.out.println("Peer is interested.");
+			//If we have less than 3 downloading peers, let this peer connect.			
+			if(ConnectionCount.uploadConnections < 3){
+				return true;
+			//If we are already connected to six people who are uploading from us, keep peer choked.
+			} else if (ConnectionCount.uploadConnections > 6){
+				return false; 
+			//read message to see if they are have messages.
+			} else {	
+				int msgIDfrPeer2 = Peer.readMessage();  			
+				//byte[] bitFieldOrHaveMsg = new byte[in.available()];
+				//in.readFully(bitFieldOrHaveMsg); /**get rid of bit field*/  			
+				if(msgIDfrPeer2 == Message.MSG_HAVE || msgIDfrPeer2 == Message.MSG_BITFIELD){
+					System.out.println("Peer has something to share! Do not choke.");
+					return true;				
+				}	
+				return false;
 			}
+		} else{
 			return false;
 		}
-		return false;
 	}
+
 
 	/**
 	 * reads in request messages and sends the piece
