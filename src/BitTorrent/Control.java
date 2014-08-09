4fde5ee84fe43eb6829e7caeed527583a4190b5c
jklimmm <lim.jewel@gmail.com>
2014-08-09 15:14:11 -0400

    created control to bring unchoke method outside upload. Need to change this so its not boolean and can accomodate optimistic unchoke

---
+++ src/BitTorrent/Control.java
@@ -0,0 +1,41 @@
+package BitTorrent;
+
+import java.io.IOException;
+
+public class Control {
+
+	//Keep track of uploaded peers, call ConnectionCount
+	//If Upload is already 6, then we cannot unchoke anyone
+	//If less than 3 download peers and peer is interested, let connect
+	//If 30 second timer wakes, choke worst peer and unchoke randomly. 
+	public static boolean unchoke() throws IOException{
+
+		//read message from peer to see if they are interested.
+		int msgIDfrPeer = Peer.readMessage();
+		if (msgIDfrPeer == Message.MSG_INTERESTED){
+			System.out.println("Peer is interested.");
+			//If we have less than 3 downloading peers, let this peer connect.			
+			if(ConnectionCount.uploadConnections < 3){
+				return true;
+			//If we are already connected to six people who are uploading from us, keep peer choked.
+			} else if (ConnectionCount.uploadConnections > 6){
+				//if timer is awake, then evaluate worst peer and unchoke random peer
+				return false; 
+			//read message to see if they are have messages.
+			} else {	
+				int msgIDfrPeer2 = Peer.readMessage();  		
+				//byte[] bitFieldOrHaveMsg = new byte[in.available()];
+				//in.readFully(bitFieldOrHaveMsg); /**get rid of bit field*/  			
+				if(msgIDfrPeer2 == Message.MSG_HAVE || msgIDfrPeer2 == Message.MSG_BITFIELD){
+					System.out.println("Peer has something to share! Do not choke.");
+					return true;
+				}	
+				return false;
+			}
+		} else{
+			//Peer.closeConnection(); because peer is not interested
+			return false;
+			
+		}
+	}
+}
--- src/BitTorrent/Upload.java
+++ src/BitTorrent/Upload.java
@@ -79,7 +79,7 @@ public class Upload implements Runnable{
 
 
 		//does not upload if not unchoked
-		boolean isUnchoked = unchoke();
+		boolean isUnchoked = Control.unchoke();
 		if(isUnchoked){
 			Message unchokeMsg = new Message(1,(byte)1); /**create unchoke message*/
 			System.out.println("Writing unchoke message to peer.");
@@ -98,42 +98,6 @@ public class Upload implements Runnable{
 		}
 
 
-	}
-
-
-	//Keep track of uploaded peers, call ConnectionCount
-	//If Upload is already 6, then we cannot unchoke anyone
-	//If less than 3 download peers and peer is interested, let connect
-	//If 30 second timer wakes, choke worst peer and unchoke randomly. 
-	public boolean unchoke() throws IOException{
-
-		//read message from peer to see if they are interested.
-		int msgIDfrPeer = Peer.readMessage();
-		if (msgIDfrPeer == Message.MSG_INTERESTED){
-			System.out.println("Peer is interested.");
-			//If we have less than 3 downloading peers, let this peer connect.			
-			if(ConnectionCount.uploadConnections < 3){
-				return true;
-			//If we are already connected to six people who are uploading from us, keep peer choked.
-			} else if (ConnectionCount.uploadConnections > 6){
-				//if timer is awake, then evaluate worst peer and unchoke random peer
-				return false; 
-			//read message to see if they are have messages.
-			} else {	
-				int msgIDfrPeer2 = Peer.readMessage();  		
-				//byte[] bitFieldOrHaveMsg = new byte[in.available()];
-				//in.readFully(bitFieldOrHaveMsg); /**get rid of bit field*/  			
-				if(msgIDfrPeer2 == Message.MSG_HAVE || msgIDfrPeer2 == Message.MSG_BITFIELD){
-					System.out.println("Peer has something to share! Do not choke.");
-					return true;
-				}	
-				return false;
-			}
-		} else{
-			//Peer.closeConnection(); because peer is not interested
-			return false;
-			
-		}
 	}
 
 
